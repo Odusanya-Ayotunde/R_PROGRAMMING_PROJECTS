ca9f156f4d2f468b8ea346ab7fd1d53c8ab6d56b
Odusanya-Ayotunde <odusanyaayotunde06@gmail.com>
2022-05-25 09:24:34 +0100

    first class

---
+++ R_CLASS_DAY_1.R
@@ -0,0 +1,41 @@
+new_name <- c()
+names <- c("james", "leekee", "zola", "tolu", "bolaji", "tumumi", "tolupej")
+for (i in names){
+if  even_name <- (nchar (name) i%%2==0){
+    else odd_name <- (nchar (name) i%%2!==0)
+  }
+}
+print (nchar (names))
+print (new_name)
+
+
+names <- c('james', 'leekee', 'zola', 'tolu', 'bolaji', 'tumumi', 'tolupej')
+even_names <- c()
+odd_names <- c()
+for (i in names){
+  if (nchar(i)%%2==0){
+    even_names <- c(even_names, i)
+  } else{
+    odd_names <- c(odd_names, i)
+  }
+}
+#print(even_names)
+#print(odd_names)
+  
+  
+names_data <- c('james', 'leekee', 'zola', 'tolu', 'bolaji', 'tumumi', 'tolupej')
+even_names <- c()
+odd_names <- c()
+for (i in names_data){
+ if (nchar(i) %%2==0){
+  even_names <- c(even_names, i)
+} else {
+  odd_names <- c(odd_names, i)
+}
+}
+  print (even_names)
+  print (odd_names)
+  
+  
+  
+  
